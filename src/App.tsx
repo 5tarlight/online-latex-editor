ed410ece8230b24b319801cf47f5e686486e268d
YEAHx4 <developerstarlight@gmail.com>
2024-09-30 16:58:15 +0900

    render equation with mathjax

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import LatexInput from "./components/LatexInput";
 import Title from "./components/Title";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function App() {
   const [value, setValue] = useState("");
+  const [fontSize, setFontSize] = useState(20);
 
   return (
     <main className="flex justify-center">
@@ -11,7 +13,19 @@ function App() {
         <Title />
         <div className="mt-12">Buttons...</div>
         <LatexInput value={value} setValue={setValue} />
-        <div className="mt-16">{value}</div>
+        <div className="mt-16">
+          <MathJaxContext>
+            <MathJax
+              style={{
+                fontSize: `${fontSize}px`,
+                color: "black",
+                backgroundColor: "white",
+                padding: "0.5rem",
+                borderRadius: "0.5rem",
+              }}
+            >{`\\(\\displaystyle ${value}\\)`}</MathJax>
+          </MathJaxContext>
+        </div>
       </div>
     </main>
   );
