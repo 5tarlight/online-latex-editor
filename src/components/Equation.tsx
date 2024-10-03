d252241c790ee86674ddbef9e3a0638fd02e2ad5
YEAHx4 <developerstarlight@gmail.com>
2024-10-03 16:25:14 +0900

    [STYLE] Align export buttons centre

--- src/components/Equation.tsx
+++ src/components/Equation.tsx
@@ -1,24 +1,30 @@
 import { MathJax, MathJaxContext } from "better-react-mathjax";
-import ExportToSvg from './Downloads/ExportToSvg';
-import ExportToPng from './Downloads/ExportToPng';
+import ExportToSvg from "./Downloads/ExportToSvg";
+import ExportToPng from "./Downloads/ExportToPng";
 
-export default function Equation({ children, fontSize }: { children: string; fontSize: number }) {
+export default function Equation({
+  children,
+  fontSize,
+}: {
+  children: string;
+  fontSize: number;
+}) {
   return (
     <div className="mt-8">
       <MathJaxContext>
         <MathJax
           style={{
             fontSize: `${fontSize}px`,
-            color: 'black',
-            backgroundColor: 'white',
-            padding: '0.5rem',
-            borderRadius: '0.5rem',
+            color: "black",
+            backgroundColor: "white",
+            padding: "0.5rem",
+            borderRadius: "0.5rem",
           }}
         >
           {`\\[ ${children} \\]`}
         </MathJax>
       </MathJaxContext>
-      <div className="flex flex-col-2 gap-2">
+      <div className="flex justify-center flex-col-2 gap-2">
         <ExportToSvg children={children} />
         <ExportToPng children={children} />
       </div>
